a29b12209c660c10f054f8f4d8b04e31d41ce530
Chimezie Edeh <princecee15@gmail.com>
2025-09-16 23:11:39 +0100

    feat: add file system checks for upload directory creation in middleware

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,5 @@
 import express from "express";
 import cors from "cors";
-import path from "node:path";
 
 import { UploadsHandler } from "./handlers";
 import { uploadsRouter } from "./routes";
--- server/src/middlewares/uploads.middleware.ts
+++ server/src/middlewares/uploads.middleware.ts
@@ -1,8 +1,14 @@
 import multer from "multer";
 import path from "node:path";
+import fs from "node:fs";
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
+    const uploadDir = path.join(__dirname, "../uploads");
+    if (!fs.existsSync(uploadDir)) {
+      fs.mkdirSync(uploadDir, { recursive: true });
+    }
+
     cb(null, path.join(__dirname, "../uploads"));
   },
   filename: (req, file, cb) => {
